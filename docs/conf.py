fe8bf680918eab807a9a1a091765baab8a8952d8
Chris Holdgraf <choldgraf@berkeley.edu>
2020-10-09 16:43:53 -0700

    blackifying docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = '2i2c Pilot Hubs Infrastructure'
-copyright = '2020, 2i2c.org'
-author = '2i2c.org'
+project = "2i2c Pilot Hubs Infrastructure"
+copyright = "2020, 2i2c.org"
+author = "2i2c.org"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,24 +28,26 @@ author = '2i2c.org'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-'sphinx.ext.intersphinx',
+    "myst_parser",
+    "sphinx.ext.intersphinx",
 ]
 
-intersphinx_mapping = {'z2jh': ("https://zero-to-jupyterhub.readthedocs.io/en/latest/", None)}
+intersphinx_mapping = {
+    "z2jh": ("https://zero-to-jupyterhub.readthedocs.io/en/latest/", None)
+}
 # -- MyST configuration ---------------------------------------------------
 myst_admonition_enable = True
 myst_deflist_enable = True
 
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,7 +55,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = "sphinx_book_theme"
 html_theme_options = {
     "single_page": True,
     "repository_url": "https://github.com/2i2c-org/pilot-hubs",
