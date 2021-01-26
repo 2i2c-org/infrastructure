8366a8ee4caa93947bc74cbe343d0820c8c377c2
GeorgianaElena <georgiana.dolocan@gmail.com>
2021-01-26 15:58:30 +0200

    Test templates for all domains

--- tests/test_custom_templates.py
+++ tests/test_custom_templates.py
@@ -2,35 +2,30 @@ import pytest
 import requests
 from bs4 import BeautifulSoup
 
-
 def test_templates_loaded(hub):
     """
     Tests if every hub has loaded their custom login template.
 
     It checks that each hub's login page has their configured institutional logo.
     """
+    hub_domains = self.spec['domain'] if isinstance(self.spec['domain'], list) else [self.spec['domain']]
 
-    hub_org_domain = (
-        self.spec["domain"]
-        if isinstance(self.spec["domain"], str)
-        else next(domain for domain in self.spec["domain"] if "2i2c" in domain)
-    )
+    for domain in hub_domains:
+        url = f'https://{domain}/hub/login'
 
-    url = f'https://{hub_org_domain}/hub/login'
+        response = requests.get(url)
+        if hub.spec["template"] == "base-hub":
+            expected_logo_img = hub.spec["config"]["jupyterhub"]["homepage"]["templateVars"]["org"]["logo_url"]
+        else:
+            expected_logo_img = hub.spec["config"]["base-hub"]["jupyterhub"]["homepage"]["templateVars"]["org"]["logo_url"]
 
-    response = requests.get(url)
-    if hub.spec["template"] == "base-hub":
-        expected_logo_img = hub.spec["config"]["jupyterhub"]["homepage"]["templateVars"]["org"]["logo_url"]
-    else:
-        expected_logo_img = hub.spec["config"]["base-hub"]["jupyterhub"]["homepage"]["templateVars"]["org"]["logo_url"]
+        has_logo = False
+        soup = BeautifulSoup(response.text, 'html.parser')
+        images = soup.find_all('img')
+        for image in images:
+            if image.has_attr("class") and image["class"] == ["hub-logo"]:
+                has_logo = True
+                assert image['src'] == expected_logo_img
 
-    has_logo = False
-    soup = BeautifulSoup(response.text, 'html.parser')
-    images = soup.find_all('img')
-    for image in images:
-        if image.has_attr("class") and image["class"] == ["hub-logo"]:
-            has_logo = True
-            assert image['src'] == expected_logo_img
-
-    if hub.spec["template"] != "ephemeral-hub":
-        assert has_logo
+        if hub.spec["template"] != "ephemeral-hub":
+            assert has_logo
