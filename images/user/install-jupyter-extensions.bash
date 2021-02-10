d969131cbb53f15d1304ec6daa3df4c498305caf
GeorgianaElena <georgiana.dolocan@gmail.com>
2021-02-10 15:58:56 +0200

    Use pip instead of conda because conda now hangs when building image

--- images/user/install-jupyter-extensions.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-jupyter labextension install --debug \
-    @jupyterlab/server-proxy@2.1.2 \
-
-# Install jupyter-contrib-nbextensions
-jupyter contrib nbextension install --sys-prefix --symlink
