7e48164192b02dbc1f93190646afcd30903afb38
YuviPanda <yuvipanda@gmail.com>
2021-03-27 02:50:01 +0530

    Specify configurator schema in deployment config
    Configurator schema is likely to be specific to particular
    deployments - although we should publish generic schemas
    too. By moving it into config, we can iterate on this faster,
    and have per-hub schemas too.

---
+++ hub-templates/images/hub/jupyterhub_configurator_config.py
@@ -0,0 +1,16 @@
+import os
+from glob import glob
+import json
+
+
+CONFIGURATOR_BASE_PATH = "/usr/local/etc/jupyterhub-configurator"
+
+schema_files = sorted(glob(os.path.join(CONFIGURATOR_BASE_PATH, '*.schema.json')))
+
+schemas = {}
+
+for sf in schema_files:
+    with open(sf) as f:
+        schemas[sf] = json.load(f)
+
+c.Configurator.schemas = schemas
