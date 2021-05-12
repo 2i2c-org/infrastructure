5c90a7641a272a5a18cf5692790d933ea5bdd2f7
YuviPanda <yuvipanda@gmail.com>
2021-05-13 00:54:42 +0530

    Document setup-efs.py script

--- kops/setup-efs.py
+++ kops/setup-efs.py
@@ -5,6 +5,9 @@ Hacky script to automate EFS setup for a given kops cluster
 1. Create an EFS file system
 2. Create a mount target with correct security groups & subnets for
    a given kops cluster
+
+Should be terraform that runs after kops cluster is created and
+fetches VPC / SG / Subnets with data sources instead.
 """
 
 import sys
@@ -23,23 +26,33 @@ def find_subnets(cluster_name, region):
 
 def find_security_groups(cluster_name, region):
     """
-    Find security groups of master and nodes
+    Find security groups of master and nodes of given cluster.
+
+    The EFS mount target needs to be available to the master node of the
+    cluster too - this is where the hub, proxy, and other core pods live.
+    The hub-share-creator also runs there.
     """
     ec2 = boto3.client('ec2', region_name=region)
     return ec2.describe_security_groups(Filters=[
         {'Name':'tag:Name', 'Values': [f'{t}.{cluster_name}' for t in ['masters', 'nodes']]}
     ])['SecurityGroups']
 
-def create_filesystem(token, name, region):
+def create_filesystem(token, cluster_name, region):
+    """
+    Create an EFS filesystem for given cluster
+
+    Sets up a mount target in the appropriate subnets with correct
+    security groups too.
+    """
     efs = boto3.client('efs', region_name=region)
-    subnets = find_subnets(name, region)
-    security_groups = find_security_groups(name, region)
+    subnets = find_subnets(cluster_name, region)
+    security_groups = find_security_groups(cluster_name, region)
     fs = efs.create_file_system(
         CreationToken=token,
         Encrypted=True,
         Backup=True,
         Tags=[
-            {'Key': 'KubernetesCluster', 'Value': name}
+            {'Key': 'KubernetesCluster', 'Value': cluster_name}
         ]
     )
     while True:
