e7d59c42d449aebb37fa5294af0c7e2c60c199ed
YuviPanda <yuvipanda@gmail.com>
2021-07-28 23:41:51 +0530

    Create AzureFile PVC / PV with Helm
    One PV can't be bound to more than one PV, so we need one
    PV per hub anyway. So we use terraform to create the secret,
    but not the PVs.
    
    Anytime we have a PVC per hub, we'll need a PV too. The
    split of NFS PVC and PV is now unbound.

--- terraform/azure/storage.tf
+++ terraform/azure/storage.tf
@@ -30,31 +30,3 @@ resource "kubernetes_secret" "homes" {
     azurestorageaccountkey  = azurerm_storage_account.homes.primary_access_key
   }
 }
-
-resource "kubernetes_persistent_volume" "homes" {
-  metadata {
-    name = "homes"
-  }
-  spec {
-    capacity = {
-      storage = "1Mi"
-    }
-    access_modes = ["ReadWriteMany"]
-
-    # See https://linux.die.net/man/8/mount.cifs for available options
-    mount_options = [
-      "uid=1000",
-      "forceuid",
-      "gid=1000",
-      "forcegid",
-      "nobrl"
-    ]
-    persistent_volume_source {
-      azure_file {
-        secret_name      = kubernetes_secret.homes.metadata[0].name
-        secret_namespace = kubernetes_namespace.homes.metadata[0].name
-        share_name       = azurerm_storage_share.homes.name
-      }
-    }
-  }
-}
