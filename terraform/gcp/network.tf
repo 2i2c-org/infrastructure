87b6fb68300305402260f69863359d132519c12e
Sarah Gibson <drsarahlgibson@gmail.com>
2021-08-03 16:51:26 +0100

    Deploy and Cloud Router and Cloud NAT to handle outbound traffic

--- terraform/gcp/network.tf
+++ terraform/gcp/network.tf
@@ -40,3 +40,23 @@ resource "google_compute_firewall" "firewall_rules" {
 
   source_ranges = ["35.235.240.0/20"]
 }
+
+resource "google_compute_router" "router" {
+  count = var.enable_private_cluster ? 1 : 0
+
+  name    = "${var.prefix}-router"
+  project = var.project_id
+  region  = var.region
+  network = google_compute_network.vpc_network[0].id
+}
+
+resource "google_compute_router_nat" "nat" {
+  count = var.enable_private_cluster ? 1 : 0
+
+  name                               = "${var.prefix}-router-nat"
+  project                            = var.project_id
+  region                             = var.region
+  router                             = google_compute_router.router[0].name
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
