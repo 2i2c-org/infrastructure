d89f506e6270ad21f5ba03bd9c00f4cd0ba5496d
Sarah Gibson <drsarahlgibson@gmail.com>
2021-12-01 13:17:46 +0000

    Use count parameter to optionally create a service principal

--- terraform/azure/service-principal.tf
+++ terraform/azure/service-principal.tf
@@ -1,19 +1,23 @@
 resource "azuread_service_principal" "service_principal" {
+  count = var.create_service_principal ? 1 : 0
+
   application_id               = var.subscription_id
   app_role_assignment_required = false
   use_existing                 = true
 }
 
 resource "azuread_service_principal_password" "service_principal_password" {
-  service_principal_id = azuread_service_principal.service_principal.object_id
+  count = var.create_service_principal ? 1 : 0
+
+  service_principal_id = azuread_service_principal.service_principal[1].object_id
 }
 
 locals{
   service_principal = {
     "tenant_id": var.tenant_id,
     "subscription_id": var.subscription_id,
-    "service_principal_id": azuread_service_principal.service_principal.object_id,
-    "service_principal_password": azuread_service_principal_password.service_principal_password.value
+    "service_principal_id": var.create_service_principal ? azuread_service_principal.service_principal[1].object_id : "",
+    "service_principal_password": var.create_service_principal ? azuread_service_principal_password.service_principal_password[1].value : ""
   }
 }
 
