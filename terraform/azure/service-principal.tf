7d47ec7aa8dd5aea72a416244d5e0d89410e085b
Sarah Gibson <drsarahlgibson@gmail.com>
2021-12-01 11:05:53 +0000

    Output service principal so it can be used to login

--- terraform/azure/service-principal.tf
+++ terraform/azure/service-principal.tf
@@ -7,3 +7,17 @@ resource "azuread_service_principal" "service_principal" {
 resource "azuread_service_principal_password" "service_principal_password" {
   service_principal_id = azuread_service_principal.service_principal.object_id
 }
+
+locals{
+  service_principal = {
+    "tenant_id": var.tenant_id,
+    "subscription_id": var.subscription_id,
+    "service_principal_id": azuread_service_principal.service_principal.object_id,
+    "service_principal_password": azuread_service_principal_password.service_principal_password.value
+  }
+}
+
+output "service_principal_config" {
+  value     = jsonencode(local.service_principal)
+  sensitive = true
+}
