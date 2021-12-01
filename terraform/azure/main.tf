2b3e314f2ea9886252a820fda9937e5725759112
Sarah Gibson <drsarahlgibson@gmail.com>
2021-12-01 11:51:12 +0000

    Revert "Remove raw kubeconfig output"
    This reverts commit ae265a2857e95fcb8b78bf6f25fd4c918d0ac734.

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -183,6 +183,12 @@ locals {
   }
 }
 
+
+output "kubeconfig" {
+  value     = azurerm_kubernetes_cluster.jupyterhub.kube_config_raw
+  sensitive = true
+}
+
 output "registry_creds_config" {
   value     = jsonencode(local.registry_creds)
   sensitive = true
