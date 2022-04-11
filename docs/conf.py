7f03063276786b3db64303fa0f3e0e751a152702
Chris Holdgraf <choldgraf@gmail.com>
2022-04-11 14:41:40 -0700

    linkcheck fix

--- docs/conf.py
+++ docs/conf.py
@@ -65,6 +65,7 @@ linkcheck_anchors = False
 linkcheck_ignore = [
     "https://howard.cloudbank.2i2c.cloud*",  # Temporarily ignore because we've changed the hub name
     "https://github.com/organizations/2i2c-org/settings/applications/new",  # Only accessible to 2i2c-org members
+    "https://docs.github.com",  # Gives 403 Forbidden errors
 ]
 
 
