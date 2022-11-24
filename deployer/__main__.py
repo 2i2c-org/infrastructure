bd34b6d34f705441f15cd5225284f46530ba45b8
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2022-11-24 12:34:43 +0200

    More comments and new token

--- deployer/__main__.py
+++ deployer/__main__.py
@@ -5,7 +5,7 @@ import deployer.deployer  # noqa: F401
 import deployer.generate.generate_aws_cluster  # noqa: F401
 import deployer.generate.generate_gcp_cluster  # noqa: F401
 import deployer.grafana.central_grafana  # noqa: F401
-import deployer.grafana.grafana_api_tokens  # noqa: F401
+import deployer.grafana.grafana_tokens  # noqa: F401
 
 from .cli_app import app
 
--- deployer/grafana/grafana_tokens.py
+++ deployer/grafana/grafana_tokens.py
@@ -1,4 +1,5 @@
 import json
+from re import T
 import subprocess
 from base64 import b64encode
 from pathlib import Path
@@ -14,6 +15,12 @@ REPO_ROOT = Path(__file__).parent.parent.parent
 
 
 def build_service_account_request_headers():
+    """
+    Build the headers needed to send requests to the Grafana service account endpoint.
+
+    Returns:
+        dict: "Accept", "Content-Type", "Authorization" headers
+    """
     # Dencrypt grafana credentials file
     grafana_username = "admin"
     grafana_password_string = subprocess.check_output(
@@ -41,9 +48,10 @@ def build_service_account_request_headers():
 
 def create_deployer_service_account(sa_endpoint, headers):
     """
-    Creates a Grafana service account with `deployer` name
+    Create a Grafana service account with the name: `deployer`
+
     Returns:
-        the service account's id
+        id: the id of the service account with the name `deployer`
     """
 
     request_body = {"name": "deployer", "role": "Admin", "isDisabled": False}
@@ -56,7 +64,8 @@ def create_deployer_service_account(sa_endpoint, headers):
     )
     if response.status_code != 200:
         print(
-            f"An error occured when creating the service account for the deployer. \nError was {response.text}."
+            "An error occured while creating the service account for the deployer.\n"
+            f"Error was {response.text}."
         )
         response.raise_for_status()
     print_colour("Successfully created a service account for the deployer!")
@@ -65,7 +74,19 @@ def create_deployer_service_account(sa_endpoint, headers):
 
 
 def get_deployer_service_account_id(sa_endpoint, headers):
-    # Assumes we don't have more than 10 service accounts created for a grafana
+    """
+    Iterate the list of service accounts and
+    retrieve the id of the service account which has the name "deployer".
+
+    Assumes:
+        the maximum number of service accounts created for a grafana is no more than 10.
+
+    Returns:
+        id: the id of the service account with the name `deployer`
+            `None` if no service account with this name was found
+    """
+
+    # Assumes we don't have more than 10 service accounts
     # FIXME if this changes
     max_sa = 10
     sa_first_page_endpoint = f"{sa_endpoint}/search?perpage={max_sa}&page=1"
@@ -73,7 +94,8 @@ def get_deployer_service_account_id(sa_endpoint, headers):
 
     if response.status_code != 200:
         print(
-            f"An error occured when retrieving the service accounts from {sa_first_page_endpoint}. \n Error was {response.text}."
+            f"An error occured when retrieving the service accounts from {sa_first_page_endpoint}.\n"
+            f"Error was {response.text}."
         )
         response.raise_for_status()
 
@@ -82,14 +104,17 @@ def get_deployer_service_account_id(sa_endpoint, headers):
         return
 
     service_accounts = response.json()["serviceAccounts"]
-    return next(sa["id"] for sa in service_accounts if sa["name"] == "deployer")
+    return next((sa["id"] for sa in service_accounts if sa["name"] == "deployer"), None)
 
 
 def get_deployer_token(sa_endpoint, sa_id, headers):
     """
+    Iterate the list of tokens created for the `deployer` service account and
+    retrieve the token with the name `deployer`.
+
     Returns:
         token, if deployer token exists
-        False otherwise
+        None otherwise
     """
     response = requests.get(
         f"{sa_endpoint}/{sa_id}/tokens",
@@ -105,50 +130,11 @@ def get_deployer_token(sa_endpoint, sa_id, headers):
     for token in response.json():
         if token["name"] == "deployer":
             return token
-    return False
+
+    return
 
 
-@app.command()
-def generate_grafana_token(
-    cluster=typer.Argument(..., help="Name of cluster where the central grafana lives")
-):
-    grafana_host = get_grafana_url(cluster)
-    sa_endpoint = f"https://{grafana_host}/api/serviceaccounts"
-    headers = build_service_account_request_headers()
-
-    sa_id = get_deployer_service_account_id(sa_endpoint, headers)
-
-    if not sa_id:
-        sa_id = create_deployer_service_account(sa_endpoint, headers)
-
-    # Check if there's a "deployer" token already
-    token = get_deployer_token(sa_endpoint, sa_id, headers)
-    overwrite = False
-    if token:
-        print_colour(
-            "Token already exists!\n"
-            f'Checking if has expired: {token["hasExpired"]}\n',
-            "yellow",
-        )
-        print_colour(
-            "Type `yes` if you want to overwrite or anything else to abort...",
-        )
-        overwrite = input()
-
-        if overwrite != "yes":
-            print_colour("Aborting...", "red")
-            return
-        # Create a token called `deployer` for the newly created `deployer` service account
-        print_colour("Deleting previous `deployer` token...", "yellow")
-        response = requests.delete(
-            f'{sa_endpoint}/{sa_id}/tokens/{token["id"]}',
-            headers=headers,
-        )
-
-    # Create a token called `deployer` for the newly created `deployer` service account
-    print_colour(
-        "Generating a token for the Grafana `deployer` service account", "yellow"
-    )
+def create_deployer_token(sa_endpoint, sa_id, headers):
     token_request_body = {"name": "deployer", "role": "Admin"}
     response = requests.post(
         f"{sa_endpoint}/{sa_id}/tokens",
@@ -158,11 +144,60 @@ def generate_grafana_token(
 
     if response.status_code != 200:
         print(
-            f"An error occured when creating the token for the deployer service account. \nError was {response.text}."
+            "An error occured when creating the token for the deployer service account.\n"
+            f"Error was {response.text}."
         )
         response.raise_for_status()
 
     print_colour("Successfully generated a token for the deployer service account")
-    token = response.json()["key"]
+
+    return response.json()["key"]
+
+
+@app.command()
+def generate_grafana_token(
+    cluster=typer.Argument(..., help="Name of cluster for who's Grafana deployment to generate a new deployer token")
+):
+    grafana_host = get_grafana_url(cluster)
+    sa_endpoint = f"https://{grafana_host}/api/serviceaccounts"
+    headers = build_service_account_request_headers()
+
+    overwrite = False
+    token = None
+
+    # Check if there's a service account called "deployer" already
+    sa_id = get_deployer_service_account_id(sa_endpoint, headers)
+    if not sa_id:
+        sa_id = create_deployer_service_account(sa_endpoint, headers)
+    else:
+        # Check if there's a "deployer" token for the "deployer service account" already
+        token = get_deployer_token(sa_endpoint, sa_id, headers)
+
+    if token:
+        print_colour(
+            "Token already exists!\n"
+            f'Checking if has expired: {token["hasExpired"]}\n',
+            "yellow",
+        )
+        print_colour(
+            "Type `yes` if you want to overwrite the existing token or anything else to abort...",
+        )
+        overwrite = input()
+        if overwrite != "yes":
+            print_colour("Aborting...", "red")
+            return
+        # Create a token called `deployer` for the newly created `deployer` service account
+        print_colour("Deleting previous `deployer` token...", "yellow")
+        response = requests.delete(
+            f'{sa_endpoint}/{sa_id}/tokens/{token["id"]}',
+            headers=headers,
+        )
+        response.raise_for_status()
+
+    # Create a token called `deployer` for the newly created `deployer` service account
+    print_colour(
+        "Generating a token for the Grafana `deployer` service account...", "yellow"
+    )
+    token = create_deployer_token(sa_endpoint, sa_id, headers)
 
     update_central_grafana_token(cluster, token)
