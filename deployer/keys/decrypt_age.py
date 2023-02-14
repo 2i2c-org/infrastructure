b981e9da1f4f800d39ab8f375e96733ffeff6980
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2023-02-14 13:01:48 +0200

    Make decrypt age a deployer sub-command

--- deployer/keys/decrypt_age.py
+++ deployer/keys/decrypt_age.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python3
 """
 Simple utility to decrypt secrets sent to `support@2i2c.org` via `age`
 """
-import argparse
 import pathlib
 import subprocess
 import sys
 import tempfile
 from contextlib import contextmanager
+
+import typer
+
+from ..cli_app import app
 
 
 @contextmanager
@@ -35,25 +37,20 @@ def decrypt_content(encrypted_contents):
         subprocess.run(cmd, input=encrypted_contents, check=True)
 
 
-def main():
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument(
-        "encrypted_file",
-        nargs="?",
-        help="Path to age-encrypted file sent by user. Leave empty to read from stdin",
+@app.command()
+def decrypt_age(
+    encrypted_file_path: str = typer.Option(
+        "",
+        help="Path to age-encrypted file sent by user. Leave empty to read from stdin.",
     )
-    args = argparser.parse_args()
-
-    if not args.encrypted_file:
+):
+    if not encrypted_file_path:
         # No file specified
         print("Paste the encrypted file contents, hit enter and then press Ctrl+D")
         encrypted_contents = sys.stdin.read().encode()
     else:
         # rb so it doesn't try to decode to utf-8, in case we have a non-armored file
-        with open(args.encrypted_file, "rb") as f:
+        with open(encrypted_file_path, "rb") as f:
             encrypted_contents = f.read()
 
     decrypt_content(encrypted_contents)
-
-
-main()
