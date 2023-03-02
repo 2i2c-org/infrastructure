da876a2e102ddfbcf9f01d5a8588517060fe64de
Pris Nasrat <pnasrat@gmail.com>
2023-03-02 10:26:58 -0500

    Enable deployer use-cluster-credentials for empty cluster
    Enable use of kubectl on clusters with no hubs
    
    See #2205

--- deployer/cluster.py
+++ deployer/cluster.py
@@ -18,7 +18,7 @@ class Cluster:
     def __init__(self, spec, config_path):
         self.spec = spec
         self.config_path = config_path
-        self.hubs = [Hub(self, hub_spec) for hub_spec in self.spec["hubs"]]
+        self.hubs = [Hub(self, hub_spec) for hub_spec in self.spec.get("hubs",[])]
         self.support = self.spec.get("support", {})
 
     @contextmanager
