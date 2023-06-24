4b63fa934b01fe4b4fd5d7bada230d97d36b1764
YuviPanda <yuvipanda@gmail.com>
2023-06-23 23:21:09 -0700

    Be consistent in naming the reports

--- reports/run.py
+++ reports/run.py
@@ -35,9 +35,6 @@ with auto_commit_git_repo(
 ) as d:
     output_base = pathlib.Path(d)
     for report in reports:
-        output_file = (output_base / report.stem / report_date.strftime('%Y/%m/%d')).with_suffix('.ipynb')
-        if output_file.exists():
-            # If an output file already exists for today, put the exact time in filename
-            output_file = (output_base / report.stem / report_date.strftime('%Y/%m/%d-%Hh%Mm%Ss%Z')).with_suffix('.ipynb')
+        output_file = (output_base / report.stem / report_date.strftime('%Y/%m/%d-%Hh%Mm%Ss%Z')).with_suffix('.ipynb')
         os.makedirs(output_file.parent, exist_ok=True)
         pm.execute_notebook(report, str(output_file))
