82d38eb33c389491fa3cf057010367ba086dca22
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2023-08-30 15:17:33 +0300

    Have the gcp generator use the common module

--- deployer/generate/generate_gcp_cluster.py
+++ deployer/generate/generate_gcp_cluster.py
@@ -1,134 +1,44 @@
-import os
-import secrets
-import string
-import subprocess
-from pathlib import Path
+"""
+Generates the ` terraform file required to create a GCP cluster
+and the required `config` directory for hubs on a GCP cluster.
 
+Generates the following files:
+- terraform/gcp/projects/<cluster_name>.tfvars`
+- `config/<cluster_name>/cluster.yaml`
+- `config/<cluster_name>/support.values.yaml`
+- `config/<cluster_name>/enc-support.secret.values.yaml`
+
+"""
 import jinja2
 import typer
 
 from ..cli_app import app
 from ..utils import print_colour
+from .common import (
+    REPO_ROOT,
+    generate_cluster_config_file,
+    generate_config_directory,
+    generate_support_files,
+)
 
-REPO_ROOT = Path(__file__).parent.parent.parent
 
-
-def generate_terraform_file(cluster_name, cluster_region, project_id, hub_type):
+def generate_terraform_file(vars):
     """
     Generates the `terraform/gcp/projects/<cluster_name>.tfvars` terraform file
     required to create a GCP cluster
     """
-    with open(REPO_ROOT / f"terraform/gcp/projects/{hub_type}-template.tfvars") as f:
+    with open(
+        REPO_ROOT / f'terraform/gcp/projects/{vars["hub_type"]}-template.tfvars'
+    ) as f:
         tfvars_template = jinja2.Template(f.read())
 
-    vars = {
-        "cluster_name": cluster_name,
-        "cluster_region": cluster_region,
-        "project_id": project_id,
-    }
-
     print_colour("Generating the terraform infrastructure file...", "yellow")
-    with open(
-        REPO_ROOT / "terraform/gcp/projects" / f"{cluster_name}.tfvars", "w"
-    ) as f:
+    tfvars_file_path = (
+        REPO_ROOT / "terraform/gcp/projects" / f'{vars["cluster_name"]}.tfvars'
+    )
+    with open(tfvars_file_path, "w") as f:
         f.write(tfvars_template.render(**vars))
-    print_colour(f"{REPO_ROOT}/terraform/gcp/projects/{cluster_name}.tfvars created")
-
-
-def generate_cluster_config_file(cluster_config_directory, vars):
-    """
-    Generates the `config/<cluster_name>/cluster.yaml` config
-    """
-    with open(REPO_ROOT / "config/clusters/templates/gcp/cluster.yaml") as f:
-        cluster_yaml_template = jinja2.Template(f.read())
-    with open(cluster_config_directory / "cluster.yaml", "w") as f:
-        f.write(cluster_yaml_template.render(**vars))
-
-
-def generate_support_files(cluster_config_directory, vars):
-    """
-    Generates files related to support components.
-
-    They are required to deploy the support chart for the cluster
-    and to configure the Prometheus instance.
-
-    Generates:
-        - `config/<cluster_name>/support.values.yaml`
-        - `config/<cluster_name>/enc-support.secret.values.yaml`
-    """
-    # Generate the suppport values file `support.values.yaml`
-    print_colour("Generating the support values file...", "yellow")
-    with open(REPO_ROOT / "config/clusters/templates/gcp/support.values.yaml") as f:
-        support_values_yaml_template = jinja2.Template(f.read())
-
-    with open(cluster_config_directory / "support.values.yaml", "w") as f:
-        f.write(support_values_yaml_template.render(**vars))
-    print_colour(f"{cluster_config_directory}/support.values.yaml created")
-
-    # Generate and encrypt prometheus credentials into `enc-support.secret.values.yaml`
-    print_colour("Generating the prometheus credentials encrypted file...", "yellow")
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    credentials = {
-        "username": "".join(secrets.choice(alphabet) for i in range(64)),
-        "password": "".join(secrets.choice(alphabet) for i in range(64)),
-    }
-    with open(
-        REPO_ROOT / "config/clusters/templates/gcp/support.secret.values.yaml"
-    ) as f:
-        support_secret_values_yaml_template = jinja2.Template(f.read())
-    with open(cluster_config_directory / "enc-support.secret.values.yaml", "w") as f:
-        f.write(support_secret_values_yaml_template.render(**credentials))
-
-    # Encrypt the private key
-    subprocess.check_call(
-        [
-            "sops",
-            "--in-place",
-            "--encrypt",
-            cluster_config_directory / "enc-support.secret.values.yaml",
-        ]
-    )
-    print_colour(
-        f"{cluster_config_directory}/enc-support.values.yaml created and encrypted"
-    )
-
-
-def generate_config_directory(
-    cluster_name, cluster_region, project_id, hub_type, hub_name
-):
-    """
-    Generates the required `config` directory for hubs on a GCP cluster
-
-    Generates the following files:
-    - `config/<cluster_name>/cluster.yaml`
-    - `config/<cluster_name>/support.values.yaml`
-    - `config/<cluster_name>/enc-support.secret.values.yaml`
-    """
-    cluster_config_directory = REPO_ROOT / "config/clusters" / cluster_name
-
-    vars = {
-        "cluster_name": cluster_name,
-        "hub_type": hub_type,
-        "cluster_region": cluster_region,
-        "project_id": project_id,
-        "hub_name": hub_name,
-    }
-
-    print_colour(
-        "Checking if cluster config directory {cluster_config_directory} exists...",
-        "yellow",
-    )
-    if os.path.exists(cluster_config_directory):
-        print_colour(f"{cluster_config_directory} already exists.")
-        return
-
-    # Create the cluster config directory and initial `cluster.yaml` file
-    os.makedirs(cluster_config_directory)
-    print_colour(f"{cluster_config_directory} created")
-    generate_cluster_config_file(cluster_config_directory, vars)
-
-    # Generate the support files
-    generate_support_files(cluster_config_directory, vars)
+    print_colour(f"{tfvars_file_path} created")
 
 
 @app.command()
@@ -147,7 +57,21 @@ def generate_gcp_cluster(
     # Automatically generate the terraform config file
     generate_terraform_file(cluster_name, cluster_region, project_id, hub_type)
 
+    # These are the variables needed by the templates used to generate the cluster config file
+    # and support files
+    vars = {
+        "cluster_name": cluster_name,
+        "hub_type": hub_type,
+        "cluster_region": cluster_region,
+        "project_id": project_id,
+        "hub_name": hub_name,
+    }
+
     # Automatically generate the config directory
-    generate_config_directory(
-        cluster_name, cluster_region, project_id, hub_type, hub_name
-    )
+    cluster_config_directory = generate_config_directory(vars)
+
+    # Create the cluster config directory and initial `cluster.yaml` file
+    generate_cluster_config_file(cluster_config_directory, vars)
+
+    # Generate the support files
+    generate_support_files(cluster_config_directory, vars)
