c544d97fe1196f553aedc4fec8ab35ce72103249
Georgiana <georgiana.dolocan@gmail.com>
2024-02-28 16:09:26 +0200

    Rephrase for clarity
    Co-authored-by: Sarah Gibson <44771837+sgibson91@users.noreply.github.com>

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -416,7 +416,7 @@ variable "hub_cloud_permissions" {
   permissions users running on those hubs should have. Currently supported are:
 
   1. allow_access_to_external_requester_pays_buckets: Allow code running in user servers from this
-     hub to identify as coming from this particular GCP project when accessing GCS buckets
+     hub to identify as coming from this particular GCP project when accessing GCS buckets in other projects
      marked as 'Requester Pays'. In this case, the egress costs will
      be borne by the project *containing the hub*, rather than the project *containing the bucket*.
      Egress costs can get quite expensive, so this is 'opt-in'.
