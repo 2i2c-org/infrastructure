6a7ccff6e5240e7e2caad2661ffab19d130a7796
Sarah Gibson <drsarahlgibson@gmail.com>
2024-07-12 14:12:18 +0100

    Refactor into functions

--- helm-charts/images/gcp-filestore-backups/gcp-filestore-backups.py
+++ helm-charts/images/gcp-filestore-backups/gcp-filestore-backups.py
@@ -7,31 +7,49 @@ from datetime import datetime, timedelta
 import jmespath
 
 
-def main(args):
-    # Get a JSON object of the filestore backups. We filter by project and region
-    # in the gcloud command.
-    filestore_backups = subprocess.check_output(
+def get_existing_backups(
+    project: str, region: str, filestore_name: str, filestore_share_name: str
+):
+    """List existing backups of a share on a filestore using the gcloud CLI.
+    We filter the backups based on:
+    - GCP project
+    - GCP region
+    - Filestore name
+    - Filestore share name
+
+    Args:
+        project (str): The GCP project the filestore is located in
+        region (str): The region the filestore is located in, e.g., us-central1
+        filestore_name (str): The name of the filestore instance
+        filestore_share_name (str): The name of the share on the filestore instance
+
+    Returns:
+        list(dict): A JSON-like object, where each dict-entry in the list describes
+            an existing backup of the filestore
+    """
+    # Get all existing backups in the selected project and region
+    backups = subprocess.check_output(
         [
             "gcloud",
             "filestore",
             "backups",
             "list",
             "--format=json",
-            f"--project={args.project}",
-            f"--region={args.region}",
+            f"--project={project}",
+            f"--region={region}",
         ],
         text=True,
     )
-    filestore_backups = json.loads(filestore_backups)
+    backups = json.loads(backups)
 
     # Filter returned backups by filestore and share names
-    filestore_backups = jmespath.search(
-        f"[?sourceFileShare == '{args.filestore_share_name}' && contains(sourceInstance, '{args.filestore_name}')]",
-        filestore_backups,
+    backups = jmespath.search(
+        f"[?sourceFileShare == '{filestore_share_name}' && contains(sourceInstance, '{filestore_name}')]",
+        backups,
     )
 
-    # Parse `createTime` to a datetime object for comparison
-    filestore_backups = [
+    # Parse `createTime` property into a datetime object for comparison
+    backups = [
         {
             k: (
                 datetime.strptime(v.split(".")[0], "%Y-%m-%dT%H:%M:%S")
@@ -40,26 +58,76 @@ def main(args):
             )
             for k, v in backup.items()
         }
-        for backup in filestore_backups
+        for backup in backups
     ]
 
-    # Generate a list of filestore backups that are less than 24 hours old
-    recent_filestore_backups = [
+    return backups
+
+
+def filter_backups_into_recent_and_old(
+    backups: list, retention_days: int, day_freq: int = 1
+):
+    """Filter the list of backups into two groups:
+    - Recently created backups that were created within our backup window,
+      defined by day_freq
+    - Out of date back ups that are older than our retention window, defined by
+      retention days
+
+    Args:
+        backups (list(dict)): A JSON-like object defining the existing backups
+            for the filestore and share we care about
+        retention_days (int): The number of days above which a backup is considered
+            to be out of date
+        day_freq (int, optional): The time period in days for which we create a
+            backup. Defaults to 1 (ie. daily backups).
+
+    Returns:
+        recent_backups (list(dict)): A JSON-like object containing all existing
+            backups with a `createTime` within our backup window
+        old_backups (list(dict)): A JSON-like object containing all existing
+            backups with a `createTime` older than our retention window
+    """
+    # Generate a list of filestore backups that are younger than our backup window
+    recent_backups = [
         backup
-        for backup in filestore_backups
-        if datetime.now() - backup["createTime"] < timedelta(days=1)
+        for backup in backups
+        if datetime.now() - backup["createTime"] < timedelta(days=day_freq)
     ]
 
     # Generate a list of filestore backups that are older than our set retention period
-    old_filestore_backups = [
+    old_backups = [
         backup
-        for backup in filestore_backups
-        if datetime.now() - backup["createTime"] > timedelta(days=args.retention_days)
+        for backup in backups
+        if datetime.now() - backup["createTime"] > timedelta(days=retention_days)
     ]
-
-    if len(recent_filestore_backups) == 0:
+    if len(old_backups) > 0:
         print(
-            f"There have been no recent backups of the filestore for project {args.project}. Creating a backup now..."
+            f"Filestore backups older than {retention_days} days have been found. They will be deleted."
+        )
+
+    return recent_backups, old_backups
+
+
+def create_backup_if_necessary(
+    backups: list,
+    filestore_name: str,
+    filestore_share_name: str,
+    project: str,
+    region: str,
+):
+    """If no recent backups have been found, create a new backup using the gcloud CLI
+
+    Args:
+        backups (list(dict)): A JSON-like object containing details of recently
+            created backups
+        filestore_name (str): The name of the Filestore instance to backup
+        filestore_share_name (str): The name of the share on the Filestore to backup
+        project (str): The GCP project within which to create a backup
+        region (str): The GCP region to create the backup in, e.g. us-central1
+    """
+    if len(backups) == 0:
+        print(
+            f"There have been no recent backups of the filestore for project {project}. Creating a backup now..."
         )
 
         subprocess.check_call(
@@ -68,11 +136,11 @@ def main(args):
                 "filestore",
                 "backups",
                 "create",
-                f"{args.filestore_name}-{args.filestore_share_name}-backup-{datetime.now().strftime('%Y-%m-%d')}",
-                f"--file-share={args.filestore_share_name}",
-                f"--instance={args.filestore_name}",
-                f"--instance-location={args.region}-b",
-                f"--region={args.region}",
+                f"{filestore_name}-{filestore_share_name}-backup-{datetime.now().strftime('%Y-%m-%d')}",
+                f"--file-share={filestore_share_name}",
+                f"--instance={filestore_name}",
+                f"--instance-location={region}-b",
+                f"--region={region}",
                 # This operation can take a long time to complete and will only take
                 # longer as filestores grow, hence we use the `--async` flag to
                 # return immediately, without waiting for the operation in progress
@@ -84,12 +152,16 @@ def main(args):
     else:
         print("Recent backup found.")
 
-    if len(old_filestore_backups) > 0:
-        print(
-            f"Filestore backups older than {args.retention_days} days have been found. They will be deleted."
-        )
 
-        for backup in old_filestore_backups:
+def delete_old_backups(backups: list, region: str):
+    """If out of date backups exist, delete them using the gcloud CLI
+
+    Args:
+        backups (list(dict)): A JSON-like object containing out of date backups
+        region (str): The GCP region the backups exist in, e.g. us-central1
+    """
+    if len(backups) > 0:
+        for backup in backups:
             subprocess.check_call(
                 [
                     "gcloud",
@@ -97,12 +169,29 @@ def main(args):
                     "backups",
                     "delete",
                     backup["name"].split("/")[-1],
-                    f"--region={args.region}",
+                    f"--region={region}",
                     "--quiet",  # Otherwise we are prompted to confirm deletion
                 ]
             )
     else:
         print("No outdated backups found.")
+
+
+def main(args):
+    filestore_backups = get_existing_backups(
+        args.project, args.region, args.filestore_name, args.filestore_share_name
+    )
+    recent_filestore_backups, old_filestore_backups = (
+        filter_backups_into_recent_and_old(filestore_backups, args.retention_days)
+    )
+    create_backup_if_necessary(
+        recent_filestore_backups,
+        args.project,
+        args.filestore_name,
+        args.filestore_share_name,
+        args.region,
+    )
+    delete_old_backups(old_filestore_backups, args.region)
 
 
 if __name__ == "__main__":
