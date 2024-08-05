5d3b436e6fb87b26eef64146e46dca9d544d815e
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2024-08-05 15:25:12 +0300

    Use IRSA to map grafana k8s sa to iam roles and then use that as Principal

--- terraform/aws/grafana-athena-iam.tf
+++ terraform/aws/grafana-athena-iam.tf
@@ -8,10 +8,15 @@ resource "aws_iam_role" "grafana_athena_role" {
     Version = "2012-10-17"
     Statement = [{
 
-      Action = ["sts:AssumeRole"]
       Effect = "Allow"
       Principal = {
-        Service = "grafana.amazonaws.com"
+        Federated = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}"
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:support:support-grafana"
+        }
       }
     }]
   })
