f1ebc431b8522c65bc14164fc595f675b4dddb72
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2025-02-28 16:17:33 +0200

    Use the same capi version that's used by jetstream's default templates

--- terraform/openstack/variables.tf
+++ terraform/openstack/variables.tf
@@ -29,6 +29,12 @@ variable "image" {
   default = "ubuntu-jammy-kube-v1.31.0-240828-1652"
 }
 
+variable "capi_helm_chart_version" {
+  type        = string
+  description = "Version of the CAPI helm chart to use"
+  default     = "0.10.1"
+}
+
 variable "notebook_nodes" {
   type = map(object({
     min : optional(number),
