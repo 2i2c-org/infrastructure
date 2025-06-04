747950bde54b7039514621e15308a998b16c8985
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2025-06-04 15:15:13 +0300

    Modify exec command to function withouth mfa too

--- deployer/commands/exec/cloud.py
+++ deployer/commands/exec/cloud.py
@@ -18,36 +18,41 @@ from deployer.cli_app import exec_app
 def aws(
     profile: str = typer.Argument(..., help="Name of AWS profile to operate on"),
     mfa_device_id: str = typer.Argument(
-        ..., help="Full ARN of MFA Device the code is from"
+        None,
+        help="Full ARN of MFA Device the code is from (leave empty if not using MFA)",
     ),
     auth_token: str = typer.Argument(
-        ..., help="6 digit 2 factor authentication code from the MFA device"
+        None,
+        help="6 digit 2 factor authentication code from the MFA device (leave empty if not using MFA)",
     ),
 ):
     """
     Exec into a shell with appropriate AWS credentials (including MFA)
     """
-    creds = json.loads(
-        subprocess.check_output(
-            [
-                "aws",
-                "sts",
-                "get-session-token",
-                "--serial-number",
-                mfa_device_id,
-                "--token-code",
-                str(auth_token),
-                "--profile",
-                profile,
-            ]
-        ).decode()
-    )
-
     env = os.environ | {
-        "AWS_ACCESS_KEY_ID": creds["Credentials"]["AccessKeyId"],
-        "AWS_SECRET_ACCESS_KEY": creds["Credentials"]["SecretAccessKey"],
-        "AWS_SESSION_TOKEN": creds["Credentials"]["SessionToken"],
+        "AWS_ACCESS_KEY_ID": "",
+        "AWS_SECRET_ACCESS_KEY": "",
+        "AWS_SESSION_TOKEN": "",
         "AWS_PROFILE": profile,
     }
+    if mfa_device_id and auth_token:
+        creds = json.loads(
+            subprocess.check_output(
+                [
+                    "aws",
+                    "sts",
+                    "get-session-token",
+                    "--serial-number",
+                    mfa_device_id,
+                    "--token-code",
+                    str(auth_token),
+                    "--profile",
+                    profile,
+                ]
+            ).decode()
+        )
+        env["AWS_ACCESS_KEY_ID"] = (creds["Credentials"]["AccessKeyId"],)
+        env["AWS_SECRET_ACCESS_KEY"] = (creds["Credentials"]["SecretAccessKey"],)
+        env["AWS_SESSION_TOKEN"] = creds["Credentials"]["SessionToken"]
 
     subprocess.check_call([os.environ["SHELL"], "-l"], env=env)
