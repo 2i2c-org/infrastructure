0b880c917515acba18fa16ca107f5efd93f6f604
Angus Hollands <goosey15@gmail.com>
2025-06-11 18:14:42 +0100

    chore: run alejandra on flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 # code-owner: @agoose77
-# This flake sets up an FSH dev-shell that installs all the required 
+# This flake sets up an FSH dev-shell that installs all the required
 # packages for running deployer, and then installs the tool in the virtual environment
 # It is not best-practice for the nix-way of distributing this code,
 # but its purpose is to get an environment up and running.
@@ -39,18 +39,18 @@
             awscli2
             azure-cli
             terraform
-	    eksctl
+            eksctl
           ]);
           runScript = "${pkgs.writeShellScriptBin "runScript" (''
-set -e
-if [[ ! -d .venv ]]; then
-  ${pkgs.python3.interpreter} -m venv .venv
-  source .venv/bin/activate
-  python -m pip install -e .[dev]
-else
-  source .venv/bin/activate
-fi
-set +e
+              set -e
+              if [[ ! -d .venv ]]; then
+                ${pkgs.python3.interpreter} -m venv .venv
+                source .venv/bin/activate
+                python -m pip install -e .[dev]
+              else
+                source .venv/bin/activate
+              fi
+              set +e
             ''
             + script)}/bin/runScript";
         })
