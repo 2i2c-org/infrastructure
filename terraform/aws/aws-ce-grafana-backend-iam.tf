a60e61df26ca7e19f90d13e5d50bb7c63e340fc8
Jenny Wong <jnywong.pro@gmail.com>
2025-07-03 15:46:05 +0100

    [nasa-ghg, prod] Increase EBS volume size (#6316)
    * Upsize EBS disk
    
    * Fix inline_policy deprecation warning
    
    * Fix filter issues warning

--- terraform/aws/aws-ce-grafana-backend-iam.tf
+++ terraform/aws/aws-ce-grafana-backend-iam.tf
@@ -20,25 +20,32 @@ resource "aws_iam_role" "aws_ce_grafana_backend_iam_role" {
       },
     }]
   })
+}
 
-  inline_policy {
-    name = "aws_ce_grafana_backend_iam_policy"
+resource "aws_iam_role_policy" "aws_ce_grafana_backend_iam_policy" {
+  count = var.enable_aws_ce_grafana_backend_iam ? 1 : 0
+  name  = "aws_ce_grafana_backend_iam_policy"
+  role  = aws_iam_role.aws_ce_grafana_backend_iam_role[count.index].name
+  # ref: https://docs.aws.amazon.com/service-authorization/latest/reference/list_awscostexplorerservice.html
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ce:Get*",
+          "ce:List*",
+        ],
+        Resource = "*",
+      },
+    ]
+  })
+}
 
-    # ref: https://docs.aws.amazon.com/service-authorization/latest/reference/list_awscostexplorerservice.html
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Effect = "Allow",
-          Action = [
-            "ce:Get*",
-            "ce:List*",
-          ],
-          Resource = "*",
-        },
-      ]
-    })
-  }
+resource "aws_iam_role_policies_exclusive" "aws_ce_grafana_backend_iam_role_policies_exclusive" {
+  count        = var.enable_aws_ce_grafana_backend_iam ? 1 : 0
+  role_name    = aws_iam_role.aws_ce_grafana_backend_iam_role[count.index].name
+  policy_names = [aws_iam_role_policy.aws_ce_grafana_backend_iam_policy[count.index].name]
 }
 
 output "aws_ce_grafana_backend_k8s_sa_annotation" {
