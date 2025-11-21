14933c1324d126d56cfbed21fa15c7c726318c27
YuviPanda <yuvipanda@gmail.com>
2025-11-21 11:28:00 -0800

    Add variable to specify what metrics & labels to keep

--- terraform/uptime-checks/main.tf
+++ terraform/uptime-checks/main.tf
@@ -27,19 +27,3 @@ terraform {
     }
   }
 }
-
-data "google_client_config" "current" { }
-
-provider "helm" {
-    kubernetes = {
-        host  = "https://${google_container_cluster.sharedservices.endpoint}"
-        token = data.google_client_config.current.access_token
-        cluster_ca_certificate = base64decode(
-          google_container_cluster.sharedservices.master_auth[0].cluster_ca_certificate,
-        )
-        exec = {
-            api_version = "client.authentication.k8s.io/v1beta1"
-            command     = "gke-gcloud-auth-plugin"
-        }
-    }
-}
