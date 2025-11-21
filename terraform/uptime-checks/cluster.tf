b6cce865b64460dae480e882849f334df397acd3
YuviPanda <yuvipanda@gmail.com>
2025-11-21 14:23:05 -0800

    Add kubernetes provider

--- terraform/uptime-checks/cluster.tf
+++ terraform/uptime-checks/cluster.tf
@@ -38,3 +38,16 @@ provider "helm" {
     }
   }
 }
+
+provider "kubernetes" {
+    host  = "https://${google_container_cluster.internaltools.endpoint}"
+    token = data.google_client_config.current.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.internaltools.master_auth[0].cluster_ca_certificate,
+    )
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "gke-gcloud-auth-plugin"
+    }
+
+}
