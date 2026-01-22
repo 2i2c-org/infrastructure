736db07c1859ac17cc32c7235d570bac97ec4ce2
jnywong <jnywong.pro@gmail.com>
2026-01-22 10:31:20 +0000

    Add query function to app

--- deployer/dev_commands/exec/cost_monitoring/cost_monitoring_app.py
+++ deployer/dev_commands/exec/cost_monitoring/cost_monitoring_app.py
@@ -5,7 +5,9 @@ Nested typer application nested under exec cost-monitoring sub-command of the de
 import json
 import subprocess
 
+import requests
 import typer
+from yarl import URL
 
 from deployer.cli_app import exec_app
 from deployer.commands.validate.config import cluster_config as validate_cluster_config
@@ -47,6 +49,77 @@ def get_cost_monitoring_pod_name(namespace: str):
 
     pod = pods["items"][0]
     return pod["metadata"]["name"]
+
+
+@cost_monitoring.command()
+def query(cluster_name: str, subpath: str, verbose: bool = True):
+    """
+    Query jupyterhub-cost-monitoring-service for cost data.
+    """
+    cluster = Cluster.from_name(cluster_name)
+    grafana_token = cluster.get_grafana_token()
+    grafana_url = URL(cluster.get_grafana_url())
+    api_url = grafana_url.with_path("api/datasources")
+    datasource = requests.get(
+        api_url.human_repr(),
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {grafana_token}",
+        },
+    )
+
+    # Get the cost-monitoring yesoreyeram-infinity-datasource UID
+    datasource_uid = datasource.json()[1]["uid"]
+    target_url = URL("http://jupyterhub-cost-monitoring.support.svc.cluster.local")
+    # See https://jupyterhub-cost-monitoring.readthedocs.io/en/latest/api/ for available endpoints
+    subpath = "total-costs"
+
+    headers = {
+        "Authorization": f"Bearer {grafana_token}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "queries": [
+            {
+                "datasource": {
+                    "type": "yesoreyeram-infinity-datasource",
+                    "uid": datasource_uid,
+                },
+                "url": str(target_url / subpath),
+                "url_options": {
+                    "method": "GET",
+                    "data": "",
+                },
+                "type": "json",
+                "source": "url",
+                "format": "table",
+                "parser": "backend",
+                "root_selector": "",
+                # Columns can change depending on data returned by endpoint – see https://github.com/2i2c-org/jupyterhub-cost-monitoring/blob/a6e49c719e8600fc2490e2fb2a77da09ce4bcd1a/dashboards/common.libsonnet#L128 for examples from existing cloud cost grafana dashboards
+                "columns": [
+                    {"selector": "date", "text": "Date", "type": "timestamp"},
+                    {"selector": "name", "text": "Name", "type": "string"},
+                    {"selector": "cost", "text": "Cost", "type": "number"},
+                ],
+            }
+        ],
+        "from": "now-1d",
+        "to": "now",
+    }
+
+    # https://grafana.com/docs/grafana-cloud/developer-resources/api-reference/http-api/data_source/#query-a-data-source
+    query_url = grafana_url.with_path("api/ds/query")
+
+    response = requests.post(
+        query_url.human_repr(), headers=headers, data=json.dumps(payload)
+    )
+
+    if verbose:
+        print(response.content.decode())
+
+    return response
 
 
 @cost_monitoring.command()
