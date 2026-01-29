bec1b4b3d08367f5157b8e748e7472b5acf62c0a
Angus Hollands <goosey15@gmail.com>
2026-01-29 11:29:31 +0000

    feat: add openstack to nix (#7519)
    * feat: add openstack to nix
    
    * fix: note about FSH
    
    I since moved away from an FSH into a regular env
    
    * refactor: use venvShellHook
    
    This is not as robust as the existing hook — it doesn't hash the
    interpreter, but that's OK.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 # code-owner: @agoose77
-# This flake sets up an FSH dev-shell that installs all the required
+# This flake sets up a dev-shell that installs all the required
 # packages for running deployer, and then installs the tool in the virtual environment
 # It is not best-practice for the nix-way of distributing this code,
 # but its purpose is to get an environment up and running.
@@ -23,10 +23,18 @@
       gdk = pkgs.google-cloud-sdk.withExtraComponents (with pkgs.google-cloud-sdk.components; [
         gke-gcloud-auth-plugin
       ]);
+      openstack = python.pkgs.toPythonApplication (
+        python.pkgs.python-openstackclient.overridePythonAttrs (oldAttrs: {
+          dependencies =
+            (oldAttrs.dependencies or [])
+            ++ [python.pkgs.python-magnumclient];
+        })
+      );
       python = pkgs.python313;
       packages =
         [
           python
+          python.pkgs.venvShellHook
         ]
         ++ (with pkgs; [
           cmake
@@ -43,38 +51,9 @@
           awscli2
           azure-cli
           terraform
+          openstack
           eksctl
         ]);
-      shellHook = ''
-        # Unset leaky PYTHONPATH
-        unset PYTHONPATH
-
-        __hash=$(echo ${python.interpreter} | sha256sum)
-
-        # Setup if not defined ####
-        if [[ ! -f ".venv/$__hash" ]]; then
-            __setup_env() {
-                # Remove existing venv
-                if [[ -d .venv ]]; then
-                    rm -r .venv
-                fi
-
-                # Stand up new venv
-                ${python.interpreter} -m venv .venv
-
-                ".venv/bin/python" -m pip install -e ".[dev]"
-
-                # Add a marker that marks this venv as "ready"
-                touch ".venv/$__hash"
-            }
-
-            __setup_env
-        fi
-        ###########################
-
-        # Activate venv
-        source .venv/bin/activate
-      '';
       env = lib.optionalAttrs pkgs.stdenv.isLinux {
         # Python uses dynamic loading for certain libraries.
         # We'll set the linker path instead of patching RPATH
@@ -82,7 +61,16 @@
       };
     in {
       devShell = pkgs.mkShell {
-        inherit env packages shellHook;
+        inherit env packages;
+
+        venvDir = "./.venv";
+        postShellHook = ''
+          unset SOURCE_DATE_EPOCH PYTHONPATH
+        '';
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH PYTHONPATH
+          pip install -e ".[dev]"
+        '';
       };
     });
 }
