f2d076a97d5fc50ae041da6e44ef419005ad88bf
Angus Hollands <goosey15@gmail.com>
2026-02-27 13:14:15 +0000

    chore: update Nix flake to fix venvs

--- flake.nix
+++ flake.nix
@@ -7,11 +7,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-helm.url = "github:NixOS/nixpkgs/9b100cfb67ccb2ff6e723b78d4ae2f9c88654a1c";
+    dev-python = {
+      url = "github:agoose77/dev-flakes/e0b0b96?dir=python";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = {
     self,
     nixpkgs,
     nixpkgs-helm,
+    dev-python,
   }: let
     forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
   in {
@@ -20,8 +25,6 @@
         inherit system;
         config.allowUnfree = true;
       };
-      inherit (pkgs) lib;
-
       # Additional nixpkgs for a particular package (helm)
       pkgs-helm = import nixpkgs-helm {
         inherit system;
@@ -29,11 +32,9 @@
 
       # Define our interpreter
       python = pkgs.python313;
-      manyLinux = pkgs.pythonManylinuxPackages.manylinux2014;
       gdk = pkgs.google-cloud-sdk.withExtraComponents (with pkgs.google-cloud-sdk.components; [
         gke-gcloud-auth-plugin
       ]);
-
       # Configure packages that need additional deps
       openstack = python.pkgs.toPythonApplication (
         python.pkgs.python-openstackclient.overridePythonAttrs (oldAttrs: {
@@ -42,12 +43,17 @@
             ++ [python.pkgs.python-magnumclient];
         })
       );
-
+      # Configure the hook for enabling venvs
+      # I think there's a way to auto-detect this, but
+      # let's worry about that another time
+      venvHook =
+        dev-python.packages.${system}.nix-ld-venv-hook.override
+        {python = python;};
       # Define our env packages (including the above)
       packages =
         [
           python
-          python.pkgs.venvShellHook
+          venvHook
         ]
         ++ (with pkgs; [
           cmake
@@ -79,7 +85,6 @@
       default = pkgs.mkShell {
         inherit packages;
         # Define additional input for patching interpreter
-        nativeBuildInputs = [pkgs.makeWrapper];
 
         venvDir = ".venv";
 
@@ -88,18 +93,10 @@
 
         # Setup venv by patching interpreter with LD_LIBRARY_PATH
         # This is required because ld does not exist on Nix systems
-        postVenvCreation = let
-          # Find the interpreter of the venv
-          interpreterSubPath = lib.path.subpath.join ["bin" (baseNameOf python.interpreter)];
-        in
-          unwantedEnvPreamble
-          # Patch the venv to find the dynamic libs
-          + ''
-            wrapProgram "$VIRTUAL_ENV/${interpreterSubPath}" --prefix "LD_LIBRARY_PATH" : "${lib.makeLibraryPath manyLinux}"
-          ''
-          +
+        postVenvCreation =
           # Install package
           ''
+            ${unwantedEnvPreamble}
             pip install -e ".[dev]"
           '';
       };
