2e7539b78234501b481f0e89c66800b47f0c576f
Jamison French <jamison@developmentseed.org>
2026-04-28 17:17:29 -0500

    feat: add additional bucket policy

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -24,6 +24,7 @@ variable "user_buckets" {
     object({
       delete_after : optional(number, null),
       archival_storageclass_after : optional(number, null),
+      bucket_policy : optional(string, null),
       tags : optional(map(string), {}),
     })
   )
@@ -41,7 +42,9 @@ variable "user_buckets" {
   2. `archival_storageclass_after` - number of days after *creation* an
      object in this bucket will be automatically transitioned to a cheaper,
      slower storageclass for cost savings. Set to null to not transition.
-  3. `tags` - bucket specific tags to be merged into the general tags variable.
+  3. `bucket_policy` - optional AWS IAM policy document JSON to be merged into
+     this bucket's policy.
+  4. `tags` - bucket specific tags to be merged into the general tags variable.
   EOT
 }
 
