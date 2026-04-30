9d052f232bd474f0aed7ca6c42c641482901b9b5
Angus Hollands <goosey15@gmail.com>
2026-04-30 18:47:34 +0100

    feat: enable alarms for iops and throughput

--- terraform/aws/ebs-volumes.tf
+++ terraform/aws/ebs-volumes.tf
@@ -18,6 +18,31 @@ resource "aws_ebs_volume" "nfs_home_dirs" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "volume_throughput_alarm" {
+  alarm_name                = "warn-volume-throughput-exceeded"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 1
+  metric_name               = "VolumeThroughputExceededCheck"
+  namespace                 = "AWS/EBS"
+  period                    = 300
+  statistic                 = "Average"
+  threshold                 = 0.7
+  alarm_description         = "This metric monitors disk throughput"
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "volume_iops_alarm" {
+  alarm_name                = "warn-volume-iops-exceeded"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 1
+  metric_name               = "VolumeIOPSExceededCheck"
+  namespace                 = "AWS/EBS"
+  period                    = 300
+  statistic                 = "Average"
+  threshold                 = 0.7
+  alarm_description         = "This metric monitors disk IOPs"
+  insufficient_data_actions = []
+}
 output "ebs_volume_id_map" {
   value = { for vol in values(aws_ebs_volume.nfs_home_dirs)[*] : vol.tags["Name"] => vol.id }
 }
