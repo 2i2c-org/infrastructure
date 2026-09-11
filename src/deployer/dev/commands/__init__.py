4ed31e158a225bc7bbb22eb26b154ef60739ae81
YuviPanda <yuvipanda@gmail.com>
2026-09-10 17:56:02 -0700

    Add `deployer exec gcx` command
    Adds a command that automatically authenticates to a cluster's
    grafana and runs [gcx](https://grafana.com/docs/grafana/latest/as-code/observability-as-code/grafana-cli/gcx/)
    commands against it. This lets you perform commandline operations
    against grafana dashboards more easily.
    
    For example, the following command deletes all Grafana dashboards
    in a given cluster with a set of names:
    
    ```bash
    deployer exec gcx nasa-cryo -- resources get dashboards -o json | jq -r '.items[] | select(.spec.title == "General cloud costs" or .spec.title == "User cloud costs" or .spec.title == "Group cloud costs") | .metadata.name' | xargs -L1 -I{} deployer exec gcx nasa-cryo -- resources delete dashboards/{}
    ```
    
    You can run this against say all AWS clusters by:
    
    1. Creating a file called `cleanup-dasboards.bash` that has the
       command above, but `nasa-cryo` is replaced with `$1`
    2. Run `deployer config get clusters --provider aws | xargs -L1 ./cleanup-dashboards.bash`
    
    `gcx` has a lot of useful commands, and you can see the docs on
    [github](https://github.com/grafana/gcx/tree/main/docs).
    
    You need to install gcx separately to get this to work

--- src/deployer/dev/commands/__init__.py
+++ src/deployer/dev/commands/__init__.py
@@ -9,6 +9,7 @@ import deployer.dev.commands.debug  # noqa: F401
 import deployer.dev.commands.develop.use_cluster_credentials  # noqa: F401
 import deployer.dev.commands.exec.cost_monitoring.cost_monitoring_app  # noqa: F401
 import deployer.dev.commands.exec.get_quota_usage  # noqa: F401
+import deployer.dev.commands.exec.gcx  # noqa: F401
 import deployer.dev.commands.exec.infra_components  # noqa: F401
 import deployer.dev.commands.exec.promql  # noqa: F401
 import deployer.dev.commands.generate.cryptnono_config  # noqa: F401
